1833423bd014842f47ace60b66634b8ce616dc65
darshil1999 <darshilaslaliya@gmail.com>
2023-01-23 23:55:41 -0600

    Added 2048 Game with Tkinter

--- 2/2 2048/main.py
+++ 2/2 2048/main.py
@@ -1,7 +1,25 @@
+# import libraries
 from tkinter import *
 from tkinter import messagebox
 import random
+
+# defining game board
 class Board:
+    # backgrounf colors
+    bg_color={
+        '2': '#eee4da',
+        '4': '#ede0c8',
+        '8': '#edc850',
+        '16': '#edc53f',
+        '32': '#f67c5f',
+        '64': '#f65e3b',
+        '128': '#edcf72',
+        '256': '#edcc61',
+        '512': '#f2b179',
+        '1024': '#f59563',
+        '2048': '#edc22e',
+    }
+    # text colors
     color={
         '2': '#776e65',
         '4': '#f9f6f2',
@@ -17,35 +35,219 @@ class Board:
     }
 
     def __init__(self):
-        self.root = Tk()
-        self.root.title("2048 Game")
-        self.gamearea = Frame(self.root)
-        self.board = []
-        self.gridCell = [[0]*4 for i in range(4)]
-        self.compress = False
-        self.merge  = False
-        self.moved = False
-        self.score = 0
+        # initializing main board window
+        self.window=Tk()
+        self.window.title('2048 Game')
+        self.gameArea=Frame(self.window,bg= 'azure3')
+        self.board=[]
+        # empty board cell 4x4 board
+        self.gridCell=[[0]*4 for i in range(4)]
+        self.compress=False
+        self.merge=False
+        self.moved=False
+        self.score=0
 
         for i in range(4):
-            rows = []
+            rows=[]
             for j in range(4):
-                l = Label(self.gamearea, text='', font=('arial', 22, 'bold'),width=4, height=2)
-                l.grid(row=i,column=j, padx=7, pady=7)
+                # initialize game area 
+                l=Label(self.gameArea,text='',bg='azure4',
+                font=('arial',22,'bold'),width=4,height=2)
+                l.grid(row=i,column=j,padx=7,pady=7)
 
-                rows.append(l)
-
+                rows.append(l);
             self.board.append(rows)
-        self.gamearea.grid()
+        self.gameArea.grid()
 
-        def reverse(self):
-            for k in range(4):
-                i = 0
-                j = 3
-                while(i<j):
-                    self.gridCell[k][i], self.gridCell[k][j] = self.gridCell[k][j], self.gridCell[k][i]
-                    i+=1
-                    j-=1
+    #  if game board is reversed
+    def reverse(self):
+        for ind in range(4):
+            i=0
+            j=3
+            while(i<j):
+                self.gridCell[ind][i],self.gridCell[ind][j]=self.gridCell[ind][j],self.gridCell[ind][i]
+                i+=1
+                j-=1
 
-                def transpose(self):
-                    pass
+    # turn the game area from column to row
+    def transpose(self):
+        self.gridCell=[list(t)for t in zip(*self.gridCell)]
+
+    def compressGrid(self):
+        self.compress=False
+        temp=[[0] *4 for i in range(4)]
+        for i in range(4):
+            cnt=0
+            for j in range(4):
+                if self.gridCell[i][j]!=0:
+                    temp[i][cnt]=self.gridCell[i][j]
+                    if cnt!=j:
+                        self.compress=True
+                    cnt+=1
+        self.gridCell=temp
+
+    # add same values to one grid cell
+    def mergeGrid(self):
+        self.merge=False
+        for i in range(4):
+            for j in range(4 - 1):
+                if self.gridCell[i][j] == self.gridCell[i][j + 1] and self.gridCell[i][j] != 0:
+                    self.gridCell[i][j] *= 2
+                    self.gridCell[i][j + 1] = 0
+                    self.score += self.gridCell[i][j]
+                    self.merge = True
+
+    # add value 2 at empty cell position
+    def random_cell(self):
+        cells=[]
+        for i in range(4):
+            for j in range(4):
+                if self.gridCell[i][j] == 0:
+                    cells.append((i, j))
+        curr=random.choice(cells)
+        i=curr[0]
+        j=curr[1]
+        self.gridCell[i][j]=2
+
+    # chcek that if values are same or different
+    # if values are different then set grid cell
+    # or add values 
+    def can_merge(self):
+        for i in range(4):
+            for j in range(3):
+                if self.gridCell[i][j] == self.gridCell[i][j+1]:
+                    return True
+        
+        for i in range(3):
+            for j in range(4):
+                if self.gridCell[i+1][j] == self.gridCell[i][j]:
+                    return True
+        return False
+
+    # defining the background color and forground color of each grid cell value 
+    def paintGrid(self):
+        for i in range(4):
+            for j in range(4):
+                if self.gridCell[i][j]==0:
+                    self.board[i][j].config(text='',bg='azure4')
+                else:
+                    self.board[i][j].config(text=str(self.gridCell[i][j]),
+                    bg=self.bg_color.get(str(self.gridCell[i][j])),
+                    fg=self.color.get(str(self.gridCell[i][j])))
+
+
+class Game:
+    def __init__(self,gamepanel):
+        self.gamepanel=gamepanel
+        self.end=False
+        self.won=False
+
+    # starting the game
+    def start(self):
+        self.gamepanel.random_cell()
+        self.gamepanel.random_cell()
+        self.gamepanel.paintGrid()
+        self.gamepanel.window.bind('<Key>', self.link_keys)
+        self.gamepanel.window.mainloop()
+    
+    # keyboard inputs
+    def link_keys(self,event):
+        if self.end or self.won:
+            return
+
+        # setting default values to false for next game
+        self.gamepanel.compress = False
+        self.gamepanel.merge = False
+        self.gamepanel.moved = False
+
+        presed_key=event.keysym
+
+        # if up key is pressed then matrix of game will be transopse first then it will check
+        # that is there any grid comression is necessary or not
+        # also if the value is same then it will be added
+        # then all the other value will be replaced with this new values
+        if presed_key=='Up':
+            self.gamepanel.transpose()
+            self.gamepanel.compressGrid()
+            self.gamepanel.mergeGrid()
+            self.gamepanel.moved = self.gamepanel.compress or self.gamepanel.merge
+            self.gamepanel.compressGrid()
+            self.gamepanel.transpose()
+
+        # if down key is pressed then matrix of game will be transopse first then it will check
+        # that reversed matrix of game and after
+        # that is there any grid comression is necessary or not
+        # also if the value is same then it will be added
+        # then all the other value will be replaced with this new values
+        elif presed_key=='Down':
+            self.gamepanel.transpose()
+            self.gamepanel.reverse()
+            self.gamepanel.compressGrid()
+            self.gamepanel.mergeGrid()
+            self.gamepanel.moved = self.gamepanel.compress or self.gamepanel.merge
+            self.gamepanel.compressGrid()
+            self.gamepanel.reverse()
+            self.gamepanel.transpose()
+
+        # if left key is pressed then it will just compress the grid
+        # then it will merge the same values in grid 
+        elif presed_key=='Left':
+            self.gamepanel.compressGrid()
+            self.gamepanel.mergeGrid()
+            self.gamepanel.moved = self.gamepanel.compress or self.gamepanel.merge
+            self.gamepanel.compressGrid()
+
+        # if right key is pressed then it will reverse the values and then it will compress
+        # and add same value grid to the right grid and then set vales
+        elif presed_key=='Right':
+            self.gamepanel.reverse()
+            self.gamepanel.compressGrid()
+            self.gamepanel.mergeGrid()
+            self.gamepanel.moved = self.gamepanel.compress or self.gamepanel.merge
+            self.gamepanel.compressGrid()
+            self.gamepanel.reverse()
+        
+        # for any other key press it will just pass on
+        else:
+            pass
+
+        self.gamepanel.paintGrid()
+        print(self.gamepanel.score)
+
+        flag=0
+        # check if there any cell is containing the 2048 value or not
+        # if there then finish the game and announce the winner
+        for i in range(4):
+            for j in range(4):
+                if(self.gamepanel.gridCell[i][j]==2048):
+                    flag=1
+                    break
+
+        if(flag==1): #found 2048
+            self.won=True
+            messagebox.showinfo('2048', message='You Wonnn!!')
+            print("won")
+            return
+
+
+        for i in range(4):
+            for j in range(4):
+                if self.gamepanel.gridCell[i][j]==0:
+                    flag=1
+                    break
+
+        # otherwise game over announcement
+        if not (flag or self.gamepanel.can_merge()):
+            self.end=True
+            messagebox.showinfo('2048','Game Over!!!')
+            print("Over")
+
+        if self.gamepanel.moved:
+            self.gamepanel.random_cell()
+        
+        self.gamepanel.paintGrid()
+    
+
+gamepanel =Board()
+game2048 = Game( gamepanel)
+game2048.start()
